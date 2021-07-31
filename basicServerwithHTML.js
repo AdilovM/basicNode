65b7a3ea7d2d5aed07b64bd14d5bfbefe54de50f
AdilovM <miras9191@gmail.com>
2021-07-31 15:34:34 -0700

    removed src directory

--- basicServerwithHTML.js
+++ basicServerwithHTML.js
@@ -19,6 +19,12 @@ http.createServer(function(req,res){
 var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase(); switch(path) {
   // a helper function, serveStaticFile, that’s doing the bulk of the work.
   // fs.readFile is an asynchronous method for reading files
+  // The function is simple: it calls fs.readFile to read the contents of the specified file.
+  // fs.readFile executes the callback function when the file has been read;
+  // if the file didn’t exist or there were permissions issues reading the file,
+  // the err variable is set, and the function returns an HTTP status code of 500
+  // indicating a server error. If the file is read successfully, the file is sent
+  // to the client with the specified response code and content type.
 case '':
   serveStaticFile(res, '/public/home.html', 'text/html');
   break;
