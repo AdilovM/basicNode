43438a2347a917c12b9a0ba017cc62d05b5cfd80
AdilovM <miras9191@gmail.com>
2021-08-15 18:29:49 -0700

    added dynamic content in about page

--- app.js
+++ app.js
@@ -1,37 +1,99 @@
+// 1 Routing
+
+// var http = require('http');
+// http.createServer(function(req,res){
+// // normalize url by removing querystring, optional
+// // trailing slash, and making it lowercase
+// var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase(); switch(path) {
+// case '':
+// res.writeHead(200, { 'Content-Type': 'text/plain' }); res.end('Homepage');
+// break;
+// case '/about':
+// res.writeHead(200, { 'Content-Type': 'text/plain' }); res.end('About');
+// break;
+// default:
+// res.writeHead(404, { 'Content-Type': 'text/plain' }); res.end('Not Found');
+// break;
+//         }
+// }).listen(3000);
+//     console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+
+
+// Serving static resources
+
+// var http = require('http'), fs = require('fs');
+// function serveStaticFile(res, path, contentType, responseCode) { 
+//   if(!responseCode) responseCode = 200; 
+//   fs.readFile(__dirname + path, function(err,data) {
+//     if(err) {
+//       res.writeHead(500, { 'Content-Type': 'text/plain' }); 
+//       res.end('500 - Internal Error');
+//     } else { 
+//       res.writeHead(responseCode,
+//         { 'Content-Type': contentType });
+//           res.end(data);
+//     } 
+//   });
+// }
+// http.createServer(function(req,res){
+// // normalize url by removing querystring, optional // trailing slash, and making lowercase
+// var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase(); 
+// switch(path) {
+//   case '':
+//     serveStaticFile(res, '/public/home.html', 'text/html'); 
+//     break;
+//   case '/about':
+//     serveStaticFile(res, '/public/about.html', 'text/html'); 
+//     break;
+//   case '/img/logo.jpg':
+//     serveStaticFile(res, '/public/img/logo.jpg','image/jpeg'); 
+//     break;
+//   default:
+//     serveStaticFile(res, '/public/404.html', 'text/html',404); 
+//     break;
+//   }
+// }).listen(3000);
+// console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+
+
+// Express
 // var express = require('express');
 // var app = express();
 // app.set('port', process.env.PORT || 3000);
-// app.get('/', function(req, res){ res.type('text/plain'); //route for App Name
-//             res.send('App Name');
-// });
-// app.get('/about', function(req, res){ //route for App Name
-//             res.type('text/plain');
-//             res.send('About App');
-// });
-
-// app.get('/about/contact', function(req, res){ //route for App Name
-//   res.type('text/plain');
-//   res.send('contact info');
-// });
-
-// app.get('/about/directions', function(req, res){ //route for App Name
-//   res.type('text/plain');
-//   res.send('directions');
-// });
 //     // custom 404 page
-// /*
-// instead of using app.get for 404 and 500 we are using app.use
-// express uses app.use to add middleware
-// middleware is a catch-all handler for anything that didnt get matched
-// by a route.
-// in express, the ORDER OF ROUTES IS IMPORTANT. If we put the 404 handler above the routes, 
-// the home page and About page would stop working: instead, those URLs would result in a 404.
-// */
-// app.use(function(req, res, next){ res.type('text/plain');//404 handler
+// app.use(function(req, res){ res.type('text/plain');
 //             res.status(404);
 //             res.send('404 - Not Found');
 // });
 //     // custom 500 page
+// app.use(function(err, req, res, next){ console.error(err.stack);
+//             res.type('text/plain');
+//             res.status(500);
+//             res.send('500 - Server Error');
+// });
+
+// app.listen(app.get('port'), function(){
+// console.log( 'Express started on http://localhost:' +
+//         app.get('port') + '; press Ctrl-C to terminate.' );
+//     });
+
+
+//Adding routes for home page and about page
+// var express = require('express');
+// var app = express();
+// app.set('port', process.env.PORT || 3000);
+// app.get('/', function(req, res){ res.type('text/plain');
+//   res.send('Meadowlark Travel');
+// });
+// app.get('/about', function(req, res){
+//   res.type('text/plain');
+//   res.send('About Meadowlark Travel');
+// });
+//     // custom 404 page
+// app.use(function(req, res, next){ res.type('text/plain');
+//   res.status(404);
+//   res.send('404 - Not Found');
+// });
 // app.use(function(err, req, res, next){ console.error(err.stack);
 //   res.type('text/plain');
 //   res.status(500);
@@ -39,45 +101,61 @@
 // });
 // app.listen(app.get('port'), function(){
 // console.log( 'Express started on http://localhost:' +
-//   app.get('port') + '; press Ctrl-C to terminate.' );
-//   });
-
-/*
-Views and Laya view is what gets delivered to the user. In the case of a website,
-that usually means HTML, though you could also deliver a PNG or a PDF, or anything
-that can be rendered by the client. For our purposes, we will consider views to be HTML.
-
-view doesnt have to static - HTML can be constructed on the fly to provide
-customized page for each request
-
---different view engines: Jade, Handlebars
---jade is "too abstract" and handlebars are less abstract
+//         app.get('port') + '; press Ctrl-C to terminate.' );
+//     });
 
 
-to install handlebars run 'npm install --save express3-handlebars'
-*/
+//Views and Layouts
+var express = require('express');
 var app = express();
+//  set up handlebars view engine
 
-var handlebars = require('express3-handlebars').create({defaultLayout : 'main'});
+var fortunes = [
+  "Conquer your fears or they will conquer you.", "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+  ];
 
+app.set('port', process.env.PORT || 3000);
+var handlebars = require('express3-handlebars') .create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
-//app.set('port', process.env.PORT || 3000);
-app.get('/', function(req, res){  //route for App Name
-            res.render('home');
+//Static Files and Views
+app.use(express.static(__dirname + '/public'));
+
+//Declaring routes (home, about, 404, 50)
+app.get('/', function(req, res) { 
+  res.render('home'); 
 });
-app.get('/about', function(req, res){ //route for App Name
-            res.render('about');
+// app.get('/about', function(req, res) {
+//   res.render('about');
+// });
+//modified the route "about" to get random fortune cookie
+app.get('/about', function(req, res){ 
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
+    // 404 catch-all handler (middleware)
+app.use(function(req, res, next){ res.status(404);
+  res.render('404');
+});
+    // 500 error handler (middleware)
+app.use(function(err, req, res, next){ 
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+app.listen(app.get('port'), function(){ console.log( 'Express started on http://localhost:' +
+  app.get('port') + '; press Ctrl-C to terminate.' );
 });
 
-app.get('/about/contact', function(req, res){ //route for App Name
-  res.type('text/plain');
-  res.send('contact info');
-});
 
-app.get('/about/directions', function(req, res){ //route for App Name
-  res.type('text/plain');
-  res.send('directions');
-});
+/* 
+Dynamic Content in Views
+
+Let’s say that on the About page, we want to deliver a 
+“virtual fortune cookie.” In our meadowlark.js file, we 
+define an array of fortune cookies: */
+
